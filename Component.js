af7f28839a810fb64fb1d785cea83fc0c886f581
DUHAN BERNHARD <duhanjavier.bernhard@swiscom.com>
2019-01-17 14:52:59 +0000

    Initial commit 17.01.2019

---
+++ Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("ZPPMCOCKPIT.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("ZPPMCOCKPIT.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
